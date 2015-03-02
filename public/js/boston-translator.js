027d22149be27ed2490b3c723b6491e5aa86cd81
Ezequiel Jose Lopez <skiel.j.lopez@gmail.com>
2015-03-01 20:37:47 -0500

    JS efects! cool ! \q q

--- public/js/boston-translator.js
+++ public/js/boston-translator.js
@@ -1,1 +1,13 @@
 // The canvas for your awesome JS code
+$('#translations-form').on('submit', function() {
+  var phrase = $('#phrase-field').val();
+  $('#translations-list').append('<li class="translation-line">' + phrase + '</li>')
+  $('#translations-block').fadeIn();
+  $('.translation-line').fadeIn();
+  $('#phrase-field').val('');
+});
+
+$('#clean-list-button').on('click', function() {
+  $('#translations-block').fadeOut();
+  $('#translations-list').empty();
+});
